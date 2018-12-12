69d4e0166becc9951284774ea00ed5ef53a002aa
Trevor Scheer <trevor.scheer@gmail.com>
2018-12-11 22:26:09 -0800

    Eject

--- src/components/__tests__/MainPage.test.tsx
+++ src/components/__tests__/MainPage.test.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {MainPage} from '../MainPage';
 import {ApolloProvider, getDataFromTree} from 'react-apollo';
 import {client} from '../../apollo/client';
 
 it('renders without crashing', async () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(
-  //   <ApolloProvider client={client}>
-  //     <MainPage />
-  //   </ApolloProvider>,
-  //   div
-  // );
-  // ReactDOM.unmountComponentAtNode(div);
   await getDataFromTree(
     <ApolloProvider client={client}>
       <MainPage />
